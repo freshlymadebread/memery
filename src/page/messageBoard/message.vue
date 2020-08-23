a6aedd8e5ed49425ea8effdea9156026ba7f7d37
fulinliu <fulinliu@tencent.com>
2020-08-23 19:29:07 +0800

    留言

--- src/page/messageBoard/message.vue
+++ src/page/messageBoard/message.vue
@@ -3,7 +3,7 @@
         <div class='board'>
             <div class='container'>
                 <div v-for='(item, index) in messageList' :key="'paper' + index" class='tray'>
-                    <div :class="'message ' + item.class" @click='checkMessage(item)' :style="(message === item ?'opacity:0;':'') + 'transform: rotate('+item.deg +'deg);'">
+                    <div :class="'message ' + item.class" @click='checkMessage(item)' :style="(message === item ?'opacity:0;':'') + 'transform: rotate('+item.deg +'deg); z-index:'+ index +''">
                         <div class='text-content'>
                         <p class='text' v-html="item.text"></p>
                         <p class='info'>于{{_getTime(new Date(item.date).valueOf(),'yyyy年MM月dd日')}}</p>
@@ -52,7 +52,7 @@ export default {
             }).then(resp=>{
                 let list = resp.data.messageList.map(item => {
                     let random = Math.ceil(Math.random()*5)
-                    let deg = Math.ceil(Math.random()*20) -10
+                    let deg = Math.ceil(Math.random()*60) -30
                     return {
                         ...item,
                         class: 'paper' + random ,
@@ -211,6 +211,133 @@ export default {
         .paper5{
             background: url('../../assets/paper/paper5.png');
             background-size: 100% 100%;
+        }
+    }
+}
+
+@media screen and (max-width: 800px) {
+    .message-board{
+        .board{
+            margin-top: -100px;
+            width: 1100px;
+            height: 760px;
+            background: transparent;
+            .container{
+                height: 50px;
+                overflow: visible;
+                margin: 150px 0px 0px 0px;
+                height: calc(100% - 150px);
+                .tray{
+                    height: 80px;
+                    .message{
+                        width: 150px;
+                        height: 150px;
+                        font-size: 10px;
+                        .text-content{
+                            padding-top: 20px;
+                            max-height: 150px;
+                            .text{
+                                margin: 0px;
+                                margin-bottom: 5px;
+                                margin-top: 3px;
+                                text-indent: 2em;
+                            }
+                            .info{
+                                margin: 0px;
+                                text-align: right;
+                            }
+                        }
+                    }
+                    .paper1{
+                        background: url('../../assets/paper/paper1.png');
+                        background-size: 100% 100%;
+                    }
+                    .paper2{
+                        background: url('../../assets/paper/paper2.png');
+                        background-size: 100% 100%;
+                    }
+                    .paper3{
+                        background: url('../../assets/paper/paper3.png');
+                        background-size: 100% 100%;
+                    }
+                    .paper4{
+                        background: url('../../assets/paper/paper4.png');
+                        background-size: 100% 100%;
+                    }
+                    .paper5{
+                        background: url('../../assets/paper/paper5.png');
+                        background-size: 100% 100%;
+                    }
+                }
+            }
+        }
+        .mark{
+            opacity: 1;
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            z-index: 9999;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            .mark-keep{
+                position: fixed;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                background: #555;
+                opacity: 0.5;
+            }
+            .message{
+                text-align: left;
+                cursor: pointer;
+                display: inline-block;
+                width: 300px;
+                height: 300px;
+                font-size: 20px;
+                transition: all 1s;
+                z-index: 9;
+                .text-content{
+                    user-select: none;
+                    padding-top: 40px;
+                    max-height: 480px;
+                    display: flex;
+                    flex-direction: column;
+                    .text{
+                        margin: 0px;
+                        margin-bottom: 10px;
+                        margin-top: 6px;
+                        text-indent: 2em;
+                    }
+                    .info{
+                        margin: 0px;
+                        text-align: right;
+                    }
+                }
+            }
+            .paper1{
+                background: url('../../assets/paper/paper1.png');
+                background-size: 100% 100%;
+            }
+            .paper2{
+                background: url('../../assets/paper/paper2.png');
+                background-size: 100% 100%;
+            }
+            .paper3{
+                background: url('../../assets/paper/paper3.png');
+                background-size: 100% 100%;
+            }
+            .paper4{
+                background: url('../../assets/paper/paper4.png');
+                background-size: 100% 100%;
+            }
+            .paper5{
+                background: url('../../assets/paper/paper5.png');
+                background-size: 100% 100%;
+            }
         }
     }
 }
